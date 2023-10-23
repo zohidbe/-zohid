730128129f7a9e9e5aac26de49d9848a09629441
zohid <zohid.xayadarov77@gmail.com>
2023-10-23 17:10:19 +0300

    salom

---
+++ js/app.js
@@ -0,0 +1,122 @@
+// alert("hello world:")
+
+// confirm ("js organishni tayormisiz")
+
+// prompt("parolv kiritung")
+
+// console.log("Hammaga salom")
+
+/* multi line commit 
+asjdsadjasff
+asfasfa
+*/ 
+
+
+// let yegindi = 4 - 2 
+
+// console.log ('yigindi')
+// console.log (yigindi)
+// console.log (yigindi)
+// console.log (yigindi)
+// console.log (yigindi)
+
+// let input = prompt ("ismingizni kiritung")
+
+// console.log (input)
+
+
+// let fName = prompt ("Ismingizni kiriting")
+
+// let lName = prompt ("Familangiznoi kiriting")
+
+// console.log (fName + " " + lName)
+
+// let age = 19;
+
+// console.log(age)
+
+// age = 30 
+ 
+// console.log (age)
+
+
+// console.log ("Nimadir nyozuv") sozdi pasga tushurish
+
+// data types
+
+// primitive
+
+// string 
+// let name =  "Eshmat cofgifhojpk[asdgsfhdfsajoofhgo'adj[hghodfhJ/N oZ'BEKISTon" => string 
+
+// Number
+// let age = 18;
+// let price = 999,99 => Number
+
+// Boleeeeen
+// let isGood = true
+// let  isBad = false => bollen 
+
+// undefined => aniqlanmagan
+// let motto = undefined 0
+// console.log (motto)
+
+// null => bosh
+// let favSport = null 
+// console.log (favSport)
+
+
+// refrense => bogliklik
+
+// Array => toplam
+// let skils = {"html" "scss" "bs" "sass"}
+// console.log (skils)
+
+// 10 ta ma'lumot olish
+// 10 ta ma'lumot olish
+// var ma'lumotlar = [];
+// var ma'lumotSoni = 10;
+// var ma'lumotSani = 0;
+
+// // Foydalanuvchidan ma'lumotlarni olish
+// while (ma'lumotSani < ma'lumotSoni) {
+//   var ma'lumot = prompt("Ma'lumot kiriting: ");
+//   ma'lumotlar.push(ma'lumot);
+//   ma'lumotSani++;
+// }
+
+// // Ma'lumotlarni console ga chiqarish
+// console.log("Olingan barcha ma'lumotlar:");
+
+// for (var i = 0; i < ma'lumotlar.length; i++) {
+//   console.log("Ma'lumot " + (i + 1) + ": " + ma'lumotlar[i]);
+// }
+
+
+
+
+
+let fName = prompt("ismingiz nima ?")
+
+let lName = prompt("Familiangizni kiriting? ")
+
+let age = prompt("yoshingizni kiriting?" )
+
+let q = prompt ("qayysi maktab da oqigansiz ?")
+
+let s = prompt ("qamalgamisiz ?")
+
+let tg = prompt ("nechinchi yil tgulgansiz ?")
+
+let h = prompt ("qaysi shaxrda yashisiz ?")
+
+let mj = prompt ( "moshinigiz bormi? ")
+
+let jk = prompt ("ovqat yiy sizmi?")
+
+let lk = prompt ("nechta hotinigiz bor?")
+
+alert ("gap yoq hamma savolga togri javob berdingiz gg")
+
+console.log (fName + " " + lName + " " + age + ""+ q + " "  + s + " "+ tg + " " + h + " "+ h + "" + mj + " "+ jk + " " +lk  )
+
